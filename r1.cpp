59489e34d21c596354d0a9b1930c743e37f9645e
Ruchir Sandeep Patel <a1881080@embg13w009.ad.adelaide.edu.au>
2024-03-20 14:19:40 +1030

    first q solverd

--- r1.cpp
+++ r1.cpp
@@ -2,15 +2,18 @@
 #include<math.h>
 
 using namespace std;
-
-int main(){
-    int sum_of_powers(int n){
+int sum_of_powers(int n){
+    int result;
         if (n == 0 || n == 1){
             return n;
         }
 
-        else 
+        else{ 
             int result = pow(sum_of_powers(n-1),n-1)+pow(sum_of_powers(n-2),n-2);
             return result;
+        }
     };
+int main(){
+    int n = 5;
+    cout<<sum_of_powers(n)<<endl;
 }
